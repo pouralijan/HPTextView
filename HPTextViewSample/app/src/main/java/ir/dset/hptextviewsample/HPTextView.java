1f93f796c1c530102706949b0960b3c414592bdd
Hassan Pouralijan <pouralijan@gmail.com>
2015-06-09 20:46:45 +0430

    Create custom class ...

---
+++ HPTextViewSample/app/src/main/java/ir/dset/hptextviewsample/HPTextView.java
@@ -0,0 +1,21 @@
+package ir.dset.hptextviewsample;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class HPTextView extends TextView {
+
+    public HPTextView(Context context) {
+        super(context);
+    }
+
+    public HPTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HPTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
